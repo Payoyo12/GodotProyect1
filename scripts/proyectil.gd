3236a5d8e01be2211d04c5078bb8334c03f06ef9
Payoyo12 <119692334+Payoyo12@users.noreply.github.com>
2024-10-23 10:03:47 +0200

    cambio de clase

--- scripts/proyectil.gd
+++ scripts/proyectil.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
-var direccion:Vector2
-var impulso:float
+var impulso = 50
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = direccion*impulso
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	velocity.y = impulso
